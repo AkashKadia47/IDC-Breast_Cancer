f694eae0cbedccd6a9b261903cb53101470745f0
Akash Kadia <akashkadia9607@gmail.com>
2023-08-16 19:10:59 -0400

    api created for the model

---
+++ api/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model("../saved_models/1")
+CLASS_NAMES = ['Negative', 'Positive']
+
+@app.get("/ping")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)  
+):
+    image = read_file_as_image(await file.read())
+    
+    img_batch = np.expand_dims(image, 0)
+
+    predictions = MODEL.predict(img_batch)
+    
+    
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return {
+        'State': predicted_class,
+        'Confidence': confidence
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app, host='localhost', port=8000)
